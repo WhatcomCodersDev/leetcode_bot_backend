c35cbbf463bd44388552c6219ef24b55e91a2d54
hanselguzman-soto <soto26938@gmail.com>
2024-06-09 14:35:19 -0700

    add route to fetch problem categories for review and rname the post one

--- app/routes/users_routes.py
+++ app/routes/users_routes.py
@@ -26,8 +26,27 @@ def get_all_problems_for_user(user_id):
     
     return jsonify(problems), 200
 
-@bp.route('/<user_id>/mark_type_for_review', methods=['POST'])
-def mark_type_for_review(user_id):
+@bp.route('/<user_id>/problem/categories/review', methods=['GET'])
+def get_problem_categories_marked_for_review(user_id):
+    if not user_id:
+        return jsonify({'error': 'ID not provided'}), 400
+
+    try:
+        review_types = leetcode_review_type_manager.get_user_review_types(user_id)
+    except Exception as e:
+        print(f"Error in get_problem_categories_marked_for_review for user {user_id}: {e}")
+        return jsonify({'error': e}), 500
+    
+    if not review_types:
+        print('No review types found')
+        return jsonify({})
+        # return jsonify({'error': 'No review types found'}), 404
+    
+    return jsonify(review_types['review_types']), 200 #just return the list, its in the form of a key-value pair
+
+
+@bp.route('/<user_id>/problem/categories/review/submit', methods=['POST'])
+def mark_problem_categories_for_review(user_id):
     data = request.json
     print(data)
 
